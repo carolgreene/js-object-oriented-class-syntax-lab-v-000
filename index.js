8ac0457a73352986deff62c184e4359d3dd48e53
Carol Greene <carolgreene3@gmail.com>
2018-10-27 16:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ class BoardMember {
   veto() {
     return 'No, I must disagree'
   }
-
+  
   approve() {
     return 'You can do that!'
+  }
+
+  doCharity() {
+    return 'I like to help people.'
   }
 }
